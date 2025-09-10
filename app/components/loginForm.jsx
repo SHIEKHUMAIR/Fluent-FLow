fd53df8754cd671158de47e50815523a174197f0
UMAIR AAMIR <umair.aamir619@gmail.com>
2025-09-10 18:19:56 +0500

    fix1.0

--- app/components/loginForm.jsx
+++ app/components/loginForm.jsx
@@ -21,7 +21,7 @@ const LoginForm = () => {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- app/components/registerForm.jsx
+++ app/components/registerForm.jsx
@@ -24,7 +24,7 @@ const RegisterForm = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("http://localhost:3000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
